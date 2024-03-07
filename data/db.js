5bf2ada0cce47365883208c0e7a5fff95f3fdb32
Plai <plai.rt.w@gmail.com>
2024-03-07 17:49:45 +0000

    more doc

--- data/db.js
+++ data/db.js
@@ -1,5 +1,8 @@
 const db = require('better-sqlite3')('expense-memo.db', { verbose: console.log });
 
+/**
+ * กำหนดค่าเริ่มต้นให้ฐานข้อมูลโดยการสร้างตารางที่จำเป็นหากยังไม่มีอยู่
+ */
 const initDb = () => {
     const createTransactionTable = db.prepare(`
       create table if not exists transactions (
@@ -9,7 +12,7 @@ const initDb = () => {
         date timestamp default current_timestamp not null
       )
     `);
-    
+
     const createAllTables = db.transaction(() => {
         createTransactionTable.run();
     });
@@ -19,6 +22,12 @@ const initDb = () => {
 
 initDb();
 
+/**
+ * บันทึกธุรกรรมลงในฐานข้อมูล
+ * @param {string} name - ชื่อของธุรกรรม
+ * @param {number} amount - จำนวนเงินของธุรกรรม
+ * @throws {Error} เมื่อเกิดข้อผิดพลาดในการบันทึกธุรกรรม
+ */
 function saveTransaction(name, amount) {
   const insertTransaction = db.prepare('insert into transactions (name, amount) values (?, ?)');
   
@@ -30,6 +39,12 @@ function saveTransaction(name, amount) {
   }
 }
 
+/**
+ * ดึงรายงานของธุรกรรมภายในช่วงเวลาที่กำหนด
+ * @param {number} lookbackDays - จำนวนวันที่ต้องการดูย้อนหลังสำหรับรายงาน
+ * @returns {Object} ออบเจ็กต์ที่ประกอบด้วยจำนวนธุรกรรมและยอดเงินรวมในช่วงเวลาที่กำหนด
+ * @throws {Error} เมื่อเกิดข้อผิดพลาดในการดึงรายงาน
+ */
 function getReport(lookbackDays) {
   const lookbackPreparedStr = `-${lookbackDays} days`;
   
--- line/messageSender.js
+++ line/messageSender.js
@@ -4,8 +4,14 @@ const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 });
 
+/**
+ * ส่งข้อความตอบกลับ
+ *
+ * @param {Object} event - อ็อบเจ็กต์เหตุการณ์
+ * @param {string} messages - ข้อความที่จะส่ง
+ */
 function reply(event, messages) {
-  return client.replyMessage({
+  client.replyMessage({
     replyToken: event.replyToken,
     messages: [{
       type: 'text',
